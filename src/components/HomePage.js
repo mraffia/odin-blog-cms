17da5a8c743c383d3a49efcacf23d0b6f33e3eaa
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-02-01 22:40:32 +0700

    Add conditionals so that HomePage will render posts based on its is_published status

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ThreeDots } from 'react-loader-spinner';
 import '../styles/HomePage.css';
 import PostCard from './PostCard.js';
 
-function HomePage({ posts, handleChoosePost, isLoading, isError }) {
+function HomePage({ posts, currentPage, handleChoosePost, isLoading, isError }) {
+  const [postStatus, setPostStatus] = useState('all');
+
+  useEffect(() => {
+    if (currentPage === "All Posts") {
+      setPostStatus('all');
+    } else if (currentPage === "Published Posts") {
+      setPostStatus(true);
+    } else if (currentPage === "Unpublished Posts") {
+      setPostStatus(false);
+    }
+  }, [currentPage])
+
   return (
     <div className="home-container">
-      <h1>All Posts</h1>
+      <h1>{currentPage}</h1>
       {isError && <div className="error-message">Something went wrong...</div>}
       {isLoading ? (
         <ThreeDots 
@@ -23,7 +35,16 @@ function HomePage({ posts, handleChoosePost, isLoading, isError }) {
       ) : (
         <div className="home-subcontainer">
           {posts.map((post, i) => {
-            if (post.is_published === false) return null;
+            if (postStatus === 'all') {
+              return (
+                <div key={i} onClick={() => handleChoosePost(post._id)}>
+                  <Link to="/post" className="post-link">
+                    <PostCard post={post} handleChoosePost={handleChoosePost} />
+                  </Link>
+                </div>
+              )
+            }
+            if (post.is_published !== postStatus) return null;
             return (
               <div key={i} onClick={() => handleChoosePost(post._id)}>
                 <Link to="/post" className="post-link">
