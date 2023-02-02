b7dd84ccc38141c6a43c18dab76b54b885ba854f
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-02-02 21:16:25 +0700

    Implement LoginPage component

---
+++ src/components/LoginPage.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { redirect } from "react-router-dom";
+import '../styles/LoginPage.css';
+
+function LoginPage() {
+  const [submitDisabled, setSubmitDisabled] = useState(false);
+  const [loginFormError, setLoginFormError] = useState(false);
+
+  function handleLogin(e) {
+    e.preventDefault();
+    setSubmitDisabled(true);
+
+    const data = new URLSearchParams();
+    for (const pair of new FormData(document.getElementById('login_form'))) {
+      data.append(pair[0], pair[1]);
+    }
+
+    fetch('https://mraffia-odin-blog-api.up.railway.app/login', {
+      method: 'POST',
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.message === 'Something is not right') {
+          setLoginFormError(true);
+        } else {
+          localStorage.setItem("userToken", data.token);
+          setLoginFormError(false);
+          document.getElementById('login_form').reset();
+          redirect("/");
+        }
+        console.log(localStorage.getItem("userToken"));
+        setSubmitDisabled(false);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Log In</h1>
+      {loginFormError ? (
+        <div className="login-form-error">Invalid username or password.</div>
+        ) : null
+      }
+      <div className="login-form">
+        <form className="login-form-container" id="login_form">
+          <label htmlFor="username">Username:</label>
+          <input id="username" type="text" placeholder="Username" name="username" required />
+
+          <label htmlFor="password">Password:</label>
+          <input id="password" type="password" name="password" required />
+
+          <button type="submit" className="btn" onClick={(e) => handleLogin(e)} disabled={submitDisabled}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
