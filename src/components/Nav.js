f4aaa54b3e69a4d5bc12d0fce8c4e86e2cd2b721
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-02-02 22:19:48 +0700

    Add logic so that Nav will display users name and log out button if the user is authenticated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
-function Nav({ handleCurrentPage }) {
+function Nav({ handleCurrentPage, handleLogout }) {
   return (
     <nav className="navbar">
       <Link to="/" className="nav-title-link">
         <div className="nav-title" onClick={() => handleCurrentPage('All Posts')}>Odin Blog CMS</div>
       </Link>
-      <ul className="nav-links">
-        <Link to="/login" className="nav-link">
-          <li>Log In</li>
-        </Link>
-      </ul>
+      {!localStorage.getItem("user_token") ? (
+        <ul className="nav-links">
+          <Link to="/login" className="nav-link">
+            <li>Log In</li>
+          </Link>
+        </ul>
+      ) : (
+        <ul className="nav-links">
+          <li className="nav-name">{localStorage.getItem("user_name")}</li>
+          <li className="nav-link" onClick={() => handleLogout()}>Log Out</li>
+        </ul>
+      )}
     </nav>
   );
 }
