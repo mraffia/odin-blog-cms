9828be7d8c1983e5cb90dccbc947f5251cd6577f
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-02-02 22:59:19 +0700

    Add logic to nav menus so that when the page is currently viewed, the menu will be highlighted

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,17 +7,17 @@ function Sidebar({ currentPage, handleCurrentPage }) {
   return (
     <div className="sidebar-container">
       <ul className="sidebar-nav">
-        <li className="sidebar-nav-menu">
+        <li className="sidebar-nav-menu" style={currentPage === "All Posts" ? {"backgroundColor": "rgb(200, 200, 200)"} : {}}>
           <Link to="/" className="sidebar-nav-link" onClick={() => handleCurrentPage('All Posts')}>
             All Posts
           </Link>
         </li>
-        <li className="sidebar-nav-menu">
+        <li className="sidebar-nav-menu" style={currentPage === "Published Posts" ? {"backgroundColor": "rgb(200, 200, 200)"} : {}}>
           <Link to="/" className="sidebar-nav-link" onClick={() => handleCurrentPage('Published Posts')}>
             Published Posts
           </Link>
         </li>
-        <li className="sidebar-nav-menu">
+        <li className="sidebar-nav-menu" style={currentPage === "Unpublished Posts" ? {"backgroundColor": "rgb(200, 200, 200)"} : {}}>
           <Link to="/" className="sidebar-nav-link" onClick={() => handleCurrentPage('Unpublished Posts')}>
             Unpublished Posts
           </Link>
@@ -25,7 +25,7 @@ function Sidebar({ currentPage, handleCurrentPage }) {
         <li className="sidebar-nav-line">
           <hr />
         </li>
-        <li className="sidebar-nav-menu">
+        <li className="sidebar-nav-menu" style={currentPage === "Create New Posts" ? {"backgroundColor": "rgb(200, 200, 200)"} : {}}>
           <Link to="/post/create" className="sidebar-nav-link" onClick={() => handleCurrentPage('Create New Posts')}>
             Create New Post
           </Link>
